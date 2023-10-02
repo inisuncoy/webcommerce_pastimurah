cc3c8931bc48f4df8e0910987a0fb501bba020e3
aryafirmansyah2 <aryafirmansyah200404@example.com>
2023-10-02 10:17:02 +0700

    menambah img alt menjadi hero

--- resources/views/pages/404/index.blade.php
+++ resources/views/pages/404/index.blade.php
@@ -3,7 +3,7 @@
 @section('pages')
 <div class="flex flex-col px-5 md:px-20 pt-10 gap-y-5">
   <div class="flex flex-col items-center justify-center gap-y-10">
-    <img src="/assets/images/hero-404.png" alt="">
+    <img src="/assets/images/hero-404.png" alt="hero">
     <div >
       <h1 class="text-2xl md:text-4xl text-center">Maaf tujuanmu tidak ada</h1>
       <p class="text-xl md:text-2xl text-center mt-2"> Coba periksa lagi tautan anda</p>
